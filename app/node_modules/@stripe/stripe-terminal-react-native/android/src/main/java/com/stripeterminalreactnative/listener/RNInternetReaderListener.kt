027ffdbabe9e3330f0f0e47bc8926a862a2d12a6
drinkmasterdrinkipl <kontakt@drinki.pl>
2026-04-02 22:14:43 +0200

    Add .gitignore, remove node_modules from tracking

--- app/node_modules/@stripe/stripe-terminal-react-native/android/src/main/java/com/stripeterminalreactnative/listener/RNInternetReaderListener.kt
+++
@@ -1,7 +0,0 @@
-package com.stripeterminalreactnative.listener
-
-import com.stripe.stripeterminal.external.callable.InternetReaderListener
-import com.stripe.stripeterminal.external.callable.ReaderDisconnectListener
-
-class RNInternetReaderListener(private val readerDisconnectListener: ReaderDisconnectListener) :
-    InternetReaderListener, ReaderDisconnectListener by readerDisconnectListener
